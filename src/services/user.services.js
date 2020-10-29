bbedb25a982f02d8e99d6e7ef9248cbef9169914
gabejoseph <gabriel.joseph4@gmail.com>
2020-10-29 13:56:08 -0400

    cleaning up services and actions

--- src/services/user.services.js
+++ src/services/user.services.js
@@ -1,11 +1,5 @@
 export const userService = {
-    login,
-    logout,
-    register,
-    getAll,
-    getById,
-    update,
-    delete: _delete
+    login
 };
 
 const BASE_URL = 'https://rails-vision-backend.herokuapp.com'
@@ -28,62 +22,6 @@ function login(email, password) {
 
             return user;
         });
-}
-
-function logout() {
-    // remove user from local storage to log user out
-    localStorage.removeItem('user');
-}
-
-function getAll() {
-    const requestOptions = {
-        method: 'GET'    
-    };
-
-    return fetch(`${BASE_URL}/users`, requestOptions).then(handleResponse);
-}
-
-function getById(id) {
-    const requestOptions = {
-        method: 'GET'
-    };
-
-    return fetch(`${BASE_URL}/users/${id}`, requestOptions).then(handleResponse);
-}
-
-function register(user) {
-    const requestOptions = {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(user)
-    };
-
-    return fetch(`${BASE_URL}/registrations`, requestOptions).then(handleResponse);
-}
-
-function update(user) {
-    const requestOptions = {
-        method: 'PATCH',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(user)
-    };
-
-    return fetch(`${BASE_URL}/users/${user.id}`, requestOptions).then(handleResponse);;
-}
-
-// prefixed function name with underscore because delete is a reserved word in javascript
-function _delete(id) {
-    const requestOptions = {
-        method: 'DELETE'
-    };
-
-    return fetch(`${BASE_URL}/users/${id}`, requestOptions).then(handleResponse);
 }
 
 function handleResponse(response) {
