552ad26ed1c9f7c87a9126a3e08f0901afffde85
gabejoseph <gabriel.joseph4@gmail.com>
2020-10-29 21:38:46 -0400

    postPayload()

--- src/services/user.services.js
+++ src/services/user.services.js
@@ -9,6 +9,8 @@ export const services = {
 const assessment = data.topics
 const weekly_count = assessment[1].weekly_count
 const payload = []
+const BASE_URL = 'https://925ee3c8729541b8407aaee847ab21b6.m.pipedream.net'
+
 console.log(payload)
 
 function transform() {
@@ -33,6 +35,7 @@ function handleLogic(user) {
             user: `${user._id}`
         }
     )
+    postPayload(payload)
 }
 
 function handleAssessment(user) {
@@ -49,45 +52,20 @@ function handleAssessment(user) {
     }
 }
 
-function postPayload() {
+function postPayload(payload) {
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload)
+    };
 
+    return fetch(`${BASE_URL}/sessions`, requestOptions)
+        .then(response => response.json)
+        .then(data => {
+            console.log(data)            
+        });
 }
 
-// const BASE_URL = 'https://rails-vision-backend.herokuapp.com'
-
-// function login(email, password) {
-//     const requestOptions = {
-//         method: 'POST',
-//         headers: {
-//             'Accept': 'application/json',
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify({ email, password })
-//     };
-
-//     return fetch(`${BASE_URL}/sessions`, requestOptions)
-//         .then(handleResponse)
-//         .then(user => {
-//             // store user details and jwt token in local storage to keep user logged in between page refreshes
-//             localStorage.setItem('user', JSON.stringify(user));
-
-//             return user;
-//         });
-// }
-
-// function handleResponse(response) {
-//     return response.text().then(text => {
-//         const data = text && JSON.parse(text);
-//         if (!response.ok) {
-//             if (response.status === 401) {
-//                 // auto logout if 401 response returned from api
-//                 logout();
-//                 // location.reload(true);
-//             }
-//             const error = (data && data.message) || response.statusText;
-//             return Promise.reject(error);
-//         }
-//         console.log(data)
-//         return data;
-//     });
-// }
