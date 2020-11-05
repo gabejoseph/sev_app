e68a4a60bd45b5213cba83e7cb6209e9f994db6a
gabejoseph <gabriel.joseph4@gmail.com>
2020-11-05 08:51:04 -0500

    updating and changing

---
+++ src/components/election_data.js
@@ -0,0 +1,77 @@
+const purdue =[{candidate:"Perdue", margin: +55,		percent: 80,	total: 16,345},
+{candidate:"Perdue", margin: +37,		percent: 83,	total: 3,292},
+{candidate:"Perdue", margin: +0.,1		percent: 84,	total: 10,858},
+{candidate:"Perdue", margin: +62,		percent: 86,	total: 5,701},
+{candidate:"Perdue", margin: +8	,	percent: 86,	total: 4,052},
+{candidate:"Perdue", margin: +48,		percent: 86,	total: 3,097},
+{candidate:"Perdue", margin: +15,		percent: 86,	total: 1,531},
+{candidate:"Perdue", margin: +43,		percent: 87,	total: 37,581},
+{candidate:"Perdue", margin: +31,		percent: 88,	total: 4,245},
+{candidate:"Perdue", margin: +12,		percent: 88,	total: 1,086},
+{candidate:"Perdue", margin: +53,		percent: 89,	total: 45,086},
+{candidate:"Perdue", margin: +79,		percent: 89,	total: 1,540},
+{candidate:"Perdue", margin: +19,		percent: 90,	total: 64,254},
+{candidate:"Perdue", margin: +48,		percent: 90,	total: 10,189},
+{candidate:"Perdue", margin: +28,		percent: 90,	total: 3,396},
+{candidate:"Perdue", margin: +49,		percent: 90,	total: 2,777},
+{candidate:"Perdue", margin: +37,		percent: 91,	total: 3,831},
+{candidate:"Perdue", margin: +73,		percent: 92,	total: 4,590},
+{candidate:"Perdue", margin: +48,		percent: 92,	total: 3,230},
+{candidate:"Perdue", margin: +5	,	percent: 93,	total: 9,983},
+{candidate:"Perdue", margin: +56,		percent: 93,	total: 8,169},
+{candidate:"Perdue", margin: +66,		percent: 93,	total: 7,639},
+{candidate:"Perdue", margin: +15,		percent: 93,	total: 4,945},
+{candidate:"Perdue", margin: +38,		percent: 93,	total: 4,588},
+{candidate:"Perdue", margin: +8	,	percent: 93,	total: 4,377},
+{candidate:"Perdue", margin: +61,		percent: 94,	total: 7,877},
+{candidate:"Perdue", margin: +63,		percent: 94,	total: 7,323},
+{candidate:"Perdue", margin: +13,		percent: 95,	total: 4,704},
+{candidate:"Perdue", margin: +15,		percent: 96,	total: 44,622},
+{candidate:"Perdue", margin: +61,		percent: 96,	total: 11,757},
+{candidate:"Perdue", margin: +41,		percent: 96,	total: 4,034},
+{candidate:"Perdue", margin: +75,		percent: 96,	total: 1,416},
+{candidate:"Perdue", margin: +56,		percent: 97,	total: 32,184},
+{candidate:"Perdue", margin: +59,		percent: 97,	total: 28,987},
+{candidate:"Perdue", margin: +43,		percent: 97,	total: 18,239},
+{candidate:"Perdue", margin: +57,		percent: 97,	total: 17,213},
+{candidate:"Perdue", margin: +66,		percent: 97,	total: 16,966},
+{candidate:"Perdue", margin: +21,		percent: 97,	total: 10,365},
+{candidate:"Perdue", margin: +59,		percent: 97,	total: 9,931},
+{candidate:"Perdue", margin: +39,		percent: 97,	total: 8,052},
+{candidate:"Perdue", margin: +80,		percent: 97,	total: 7,598},
+{candidate:"Perdue", margin: +41,		percent: 97,	total: 4,005},
+{candidate:"Perdue", margin: +14,		percent: 97,	total: 1,545},
+{candidate:"Perdue", margin: +10,		percent: 98,	total: 66,893},
+{candidate:"Perdue", margin: +40,		percent: 98,	total: 53,921},
+{candidate:"Perdue", margin: +67,		percent: 98,	total: 15,149},
+{candidate:"Perdue", margin: +53,		percent: 98,	total: 14,560},
+{candidate:"Perdue", margin: +56,		percent: 98,	total: 12,622},
+{candidate:"Perdue", margin: +6	,	percent: 98,	total: 12,341},
+{candidate:"Perdue", margin: +19,		percent: 98,	total: 11,437},
+{candidate:"Perdue", margin: +72,		percent: 98,	total: 10,644},
+{candidate:"Perdue", margin: +1.7		percent: 98, total: 9,265},
+{candidate:"Perdue", margin: +48,		percent: 98,	total: 9,208},
+{candidate:"Perdue", margin: +76,		percent: 98,	total: 8,940},
+{candidate:"Perdue", margin: +24,		percent: 98,	total: 6,955},
+{candidate:"Perdue", margin: +11,		percent: 98,	total: 5,102},
+{candidate:"Perdue", margin: +39,		percent: 98,	total: 3,023}]
+
+Ossoff +34		49%	27,892
+Ossoff +12		80%	106,204
+Ossoff +71		83%	102,605
+Ossoff +22		84%	69,799
+Ossoff +34		85%	85,560
+Ossoff +16		86%	1,941
+Ossoff +17		86%	902
+Ossoff +10		88%	374,592
+Ossoff +64		88%	354,950
+Ossoff +22		88%	79,268
+Ossoff +20		88%	4,592
+Ossoff +40		89%	43,216
+Ossoff +18		90%	3,447
+Ossoff +13		91%	2,171
+Ossoff +25		92%	68,067
+Ossoff +41		92%	4,094
+Ossoff +16		96%	403,329
+Ossoff +10		96%	2,642
+Ossoff +11		98%	53,722
